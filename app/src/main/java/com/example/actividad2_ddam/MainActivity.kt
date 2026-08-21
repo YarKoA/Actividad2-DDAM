7e184ad30d45c7ddf96349e5ba8dd0a9e09a4a13
Actividad2 <20richard06@gmail.com>
2026-08-21 11:43:50 -0600

    Parte hecha por Ricardo

--- app/src/main/java/com/example/actividad2_ddam/MainActivity.kt
+++ app/src/main/java/com/example/actividad2_ddam/MainActivity.kt
@@ -13,15 +13,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.actividad2_ddam.ui.theme.Actividad2DDAMTheme
 
+// --- Punto 1:las funciones-
+// Por: Ricardo
+
+// Función botón login
+fun autenticarUsuario(usuario: String, esGoogle: Boolean): Boolean {
+    return usuario.isNotBlank() && (esGoogle || usuario.length >= 4)
+}
+
+// Función crear actividad
+fun crearActividad(titulo: String, hora: String, dia: String): String {
+    return "Actividad '$titulo' creada para el $dia a las $hora."
+}
+
+
+// Lo que falta
+// Punto 2: Colección de datos
+// Punto 3: Manejo de excepciones y null safety
+// Punto 4: Clase, Objeto e Interfaz
+// Punto 5: Función lambda / orden superior
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Pruebas rápidas en consola para checar que mis funciones si sirvan
+        val loginOk = autenticarUsuario("Ricardo", true)
+        val nuevaTarea = crearActividad("Salir a trotar", "10:30 am", "Vie")
+        println("Login estado: $loginOk | $nuevaTarea")
+
         setContent {
             Actividad2DDAMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ricardo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
